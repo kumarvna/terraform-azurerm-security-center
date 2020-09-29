af606495d279706633f07e1aca056e91b56e7752
Kumaraswamy Vithanala <36370570+kumarvna@users.noreply.github.com>
2020-09-29 22:35:27 +0530

    initial commit

--- examples/main.tf
+++ examples/main.tf
@@ -3,7 +3,15 @@ module "security-center" {
   // version = "1.0.0"
   source = "../"
 
+  # Resource Group, location, log analytics details
+  resource_group_name          = "rg-shared-westeurope-01"
   log_analytics_workspace_name = "loganalytics-we-sharedtest"
 
-
+  # Subscription Security Center contact
+  security_center_contacts = {
+    email               = "abc@xyz.com, xyz@abc.com"
+    phone               = "+919765800967"
+    alert_notifications = true
+    alerts_to_admins    = true
+  }
 }
